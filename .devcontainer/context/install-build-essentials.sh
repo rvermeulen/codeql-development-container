e747918d75a2a7d471cb8da280d4c8f1189a7f43
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2020-07-01 17:13:28 +0200

    Add NodeJS to build essentials list
    NodeJS is a dependency of the Javascript extractor.
    Install stable NodeJS through nodesource PPA

--- .devcontainer/context/install-build-essentials.sh
+++ .devcontainer/context/install-build-essentials.sh
@@ -4,13 +4,18 @@ set -e
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin/
 
+# Add PPA for nodejs 12
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+
 # Create the following directory structure to prevent symbolic link errors when installing openjdk
 mkdir -p /usr/share/man/man1
 DEBIAN_FRONTEND=noninteractive apt-get -yq install \
     openjdk-11-jdk-headless maven \
     build-essential \
-    python python-pip python3 python3-pip python3-venv
+    python python-pip python3 python3-pip python3-venv \
+    nodejs
 
 curl https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz --output go1.14.4.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
 rm go1.14.4.linux-amd64.tar.gz
+
