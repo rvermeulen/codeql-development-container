2ed499fc0dceddc4852c98357c96b6d8a7d2d9e0
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2020-09-01 15:12:23 +0000

    Configurable QL repo reference and bumped version

--- .devcontainer/context/install-codeql.sh
+++ .devcontainer/context/install-codeql.sh
@@ -10,6 +10,15 @@ echo Cloning CodeQL repositories...
 mkdir $HOME/codeql-home
 git clone https://github.com/github/codeql.git $HOME/codeql-home/codeql-repo
 git clone https://github.com/github/codeql-go.git $HOME/codeql-home/codeql-repo/codeql-go-repo
+if [ "$2" != "head" ]; then
+    echo "Switching to CodeQL repository reference $2"
+    pushd $HOME/codeql-home/codeql-repo
+    git checkout "$2"
+    popd
+    pushd $HOME/codeql-home/codeql-repo/codeql-go-repo
+    git checkout "$2"
+    popd
+fi
 echo Unzipping CodeQL $1...
 unzip /tmp/codeql.zip -d $HOME/codeql-home/
 mv $HOME/codeql-home/codeql $HOME/codeql-home/codeql-cli
